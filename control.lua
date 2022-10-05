f7c62511ec053397fe4a1a24717758876ad94e06
Johnson <johnssoft@ya.ru>
2022-10-06 02:12:25 +0500

    попытка в моддинг xD

---
+++ control.lua
@@ -0,0 +1,1 @@
+require("stage.control.events.gui-events")
---
+++ data.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "virtual-signal",
+        name = "jei_main_button",
+        icon = "__just-enough-items__/graphics/gui/jei_main_button.png",
+        icon_size = 48,
+        subgroup = "virtual-signal-utility",
+        order = "j[jei]",
+    },
+})
